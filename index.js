193eb5034b262c21481a3e7332bfa3ac96512047
Stephano <stephanokparaskeva@gmail.com>
2020-05-20 17:23:41 +0100

    fixes import errors

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-export * from "./src/index";
+import StickyScrollView from "./src/StickyScrollView";
+import StickyFlatList from "./src/StickyFlatList";
+import { StickyManager } from "./src/StickyManager";
+import StickyContent from "./src/StickyContent";
+import StickyContainer from "./src/StickyContainer";
+
+export {
+  StickyScrollView,
+  StickyFlatList,
+  StickyManager,
+  StickyContent,
+  StickyContainer,
+};
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StickyContainer } from "./StickyContainer";
-export { default as StickyContent } from "./StickyContent";
-export { default as StickyFlatList } from "./StickyFlatList";
-export { default as StickyScrollView } from "./StickyScrollView";
